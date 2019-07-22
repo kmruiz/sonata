5b65d8dfa9544ee95576ec0d4aa14e925e962f2f
Kevin Mas Ruiz <masruizkevin@gmail.com>
2019-07-22 21:40:07 +0200

    Remove unused REPL

--- src/main/java/io/sonata/lang/cli/Bootstrap.java
+++ src/main/java/io/sonata/lang/cli/Bootstrap.java
@@ -1,23 +1,17 @@
 package io.sonata.lang.cli;
 
 import io.sonata.lang.cli.command.Compile;
-import io.sonata.lang.cli.command.REPL;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 
 public class Bootstrap {
     public static void main(String[] args) throws ArgumentParserException {
         var parser = ArgumentParsers.newFor("snc").build();
-        parser.addArgument("-i", "--interactive").type(Boolean.class).setDefault(false);
         parser.addArgument("input").nargs("*");
         parser.addArgument("-o", "--output");
 
         var namespace = parser.parseArgs(args);
 
-        if (namespace.getBoolean("interactive")) {
-            REPL.execute();
-        } else {
-            Compile.execute(namespace.getList("input"), namespace.getString("output"));
-        }
+        Compile.execute(namespace.getList("input"), namespace.getString("output"));
     }
 }
