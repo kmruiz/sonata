1ab7c1acbcdf6a8d2da92aefca6aefb06d58f7e4
Kevin Mas Ruiz <masruizkevin@gmail.com>
2019-11-18 20:33:02 +0100

    Show compile time

--- src/main/java/io/sonata/lang/cli/command/Compile.java
+++ src/main/java/io/sonata/lang/cli/command/Compile.java
@@ -2,7 +2,6 @@ package io.sonata.lang.cli.command;
 
 import io.reactivex.Flowable;
 import io.reactivex.Single;
-import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.ReplaySubject;
 import io.reactivex.subjects.Subject;
 import io.sonata.lang.analyzer.Analyzer;
@@ -19,6 +18,8 @@ import io.sonata.lang.tokenizer.Tokenizer;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
 
@@ -28,6 +29,8 @@ import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 
 public class Compile {
     public static void execute(List<String> files, String output) throws Exception {
+        Instant startingTime = Instant.now();
+
         SymbolMap symbolMap = new SymbolMap(new HashMap<>());
         BackendVisitor visitor = new BackendVisitor(JSBackend::new);
         Tokenizer tokenizer = new Tokenizer();
@@ -41,13 +44,11 @@ public class Compile {
         RequiresNodeNotifier notifier = new RxRequiresNodeNotifier(requires);
 
         var byteCode = Flowable.fromIterable(files)
-                .subscribeOn(Schedulers.io())
                 .map(Paths::get)
                 .map(Source::fromPath)
                 .concatWith(Single.fromSupplier(Source::endOfProgram))
                 .concatWith(requires.toFlowable(BUFFER))
                 .flatMap(Source::read)
-                .subscribeOn(Schedulers.computation())
                 .flatMap(tokenizer::process)
                 .reduce(Parser.initial(notifier), Parser::reduce)
                 .flatMap(analyzer::apply)
@@ -55,5 +56,9 @@ public class Compile {
                 .blockingGet();
 
         Files.write(Paths.get(output), byteCode, CREATE, TRUNCATE_EXISTING);
+
+        Instant endingTime = Instant.now();
+        String duration = Duration.between(startingTime, endingTime).toString();
+        System.out.println("Compiled everything in " + duration);
     }
 }
