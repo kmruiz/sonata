1a0498eab7cce5d296a9ceb14d9ec604d7c22ef9
Kevin Mas Ruiz <masruizkevin@gmail.com>
2019-12-31 02:52:27 +0100

    Remove obsolete tests

--- src/test/java/io/sonata/lang/e2e/concurrency/ConcurrencyTest.java
+++ src/test/java/io/sonata/lang/e2e/concurrency/ConcurrencyTest.java
@@ -4,6 +4,7 @@ import io.sonata.lang.e2e.NodeDockerTest;
 import org.junit.jupiter.api.Test;
 
 public class ConcurrencyTest extends NodeDockerTest {
+
     @Test
     public void shouldRunBothEntitiesInParallel() throws Exception {
         assertResourceScriptOutputs("doing things asynchronously\n0> ping\n0> pong\n1> ping\n1> pong\nping end\npong end", "concurrency/ping-pong");
