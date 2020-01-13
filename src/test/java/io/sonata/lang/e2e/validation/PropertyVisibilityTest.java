8675be0ce749a0a52cfde3ce8eb40e1a55d75e72
Kevin Mas Ruiz <masruizkevin@gmail.com>
2020-01-14 00:21:50 +0100

    Forbid value class to entity property access

--- src/test/java/io/sonata/lang/e2e/validation/PropertyVisibilityTest.java
+++ src/test/java/io/sonata/lang/e2e/validation/PropertyVisibilityTest.java
@@ -8,4 +8,9 @@ public class PropertyVisibilityTest extends E2ETest {
     public void anEntityShouldNotBeAbleToAccessPropertiesOfAnotherEntityEvenIfItsTheSameType() {
         assertSyntaxError("It's forbidden to access properties of another entity instance.", "validation/properties/entity-to-entity");
     }
+
+    @Test
+    public void aValueObjectShouldNotBeAbleToAccessEntityProperties() {
+        assertSyntaxError("It's forbidden to access properties of another entity instance.", "validation/properties/value-to-entity");
+    }
 }
