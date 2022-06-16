858ef7628a064f898359c78d9c1942d7ca9b9d65
Kevin <kevin.mas@hey.com>
2022-06-17 01:35:01 +0200

    Add selector info to fields

--- type-system/src/memory/internal_modeler.test.cpp
+++ type-system/src/memory/internal_modeler.test.cpp
@@ -9,7 +9,7 @@ TEST(internal_modeler, merges_two_boolean_fields_into_a_bitbag_with_padding) {
     auto registry = std::make_shared<type_registry>();
     internal_modeler modeler(64, registry);
 
-    auto entity = registry->resolve("example");
+    auto entity = registry->resolve("example_entity");
     auto boolean = registry->resolve("boolean");
 
     entity->kind = type_kind::ENTITY;
@@ -19,9 +19,9 @@ TEST(internal_modeler, merges_two_boolean_fields_into_a_bitbag_with_padding) {
 
     modeler.model_type(entity);
 
+    auto bb = std::get<bit_bag>(entity->layout.storages[0]);
     ASSERT_EQ(entity->layout.type, layout_type::STATIC);
 
-    auto bb = std::get<bit_bag>(entity->layout.storages[0]);
     ASSERT_EQ(bb.size, 8);
     ASSERT_EQ(bb.reservations[0].bits, 1);
     ASSERT_EQ(bb.reservations[0].type, bit_bag_reservation_type::BOOLEAN);
