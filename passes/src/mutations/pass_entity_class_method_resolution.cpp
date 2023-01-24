352b164827e2a6bba48c00f188cde648d303a481
Kevin <kevin.mas@hey.com>
2023-01-24 23:05:06 +0100

    Spawn syntax

--- passes/src/mutations/pass_entity_class_method_resolution.cpp
+++ passes/src/mutations/pass_entity_class_method_resolution.cpp
@@ -11,25 +11,47 @@ namespace scc::passes::mutations {
 
     pass_entity_class_method_resolution::~pass_entity_class_method_resolution() = default;
 
+    static node_ref recursive_iterate(node_ref node) {
+        if (std::dynamic_pointer_cast<nspawn_entity>(node)) {
+            auto mcall = std::dynamic_pointer_cast<nspawn_entity>(node);
+            auto nfcall = std::make_shared<nfunction_call>();
+
+            auto fcallid = std::make_shared<nidentifier>();
+            fcallid->name = mcall->entity_name + "_spawn";
+
+            nfcall->left = fcallid;
+            nfcall->arguments.insert(nfcall->arguments.begin(), mcall->arguments.cbegin(), mcall->arguments.cend());
+
+            return nfcall;
+        } else if (std::dynamic_pointer_cast<nmethod_call>(node)) {
+            auto mcall = std::dynamic_pointer_cast<nmethod_call>(node);
+            auto nfcall = std::make_shared<nfunction_call>();
+
+            auto fcallid = std::make_shared<nidentifier>();
+            fcallid->name = "EntityTest_" + mcall->method;
+
+            nfcall->left = fcallid;
+            nfcall->arguments.insert(nfcall->arguments.begin(), mcall->arguments.cbegin(), mcall->arguments.cend());
+
+            nfcall->arguments.push_front(mcall->left);
+            return nfcall;
+        } else if (std::dynamic_pointer_cast<nlet>(node)) {
+            auto mlet = std::dynamic_pointer_cast<nlet>(node);
+            if (mlet->expression.has_value()) {
+                auto old_expr = mlet->expression.value();
+                auto new_expr = recursive_iterate(old_expr);
+                mlet->expression = std::dynamic_pointer_cast<expression>(new_expr);
+            }
+        }
+
+        return node;
+    }
+
     void pass_entity_class_method_resolution::execute(ast::ast_root &root) const {
         list<node_ref> new_children;
 
         for (auto &child : root->children) {
-            if (std::dynamic_pointer_cast<nmethod_call>(child)) {
-                auto mcall = std::dynamic_pointer_cast<nmethod_call>(child);
-                auto nfcall = std::make_shared<nfunction_call>();
-
-                auto fcallid = std::make_shared<nidentifier>();
-                fcallid->name = "EntityTest_" + mcall->method;
-
-                nfcall->left = fcallid;
-                nfcall->arguments.insert(nfcall->arguments.begin(), mcall->arguments.cbegin(), mcall->arguments.cend());
-
-                nfcall->arguments.push_front(mcall->left);
-                new_children.push_back(nfcall);
-            } else {
-                new_children.push_back(child);
-            }
+            new_children.push_back(recursive_iterate(child));
         }
 
         root->children = new_children;
