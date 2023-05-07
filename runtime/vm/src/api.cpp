2cfc4b44f1ce8c2458a3ca87d6329afcc9c8d90b
Kevin <kevin.mas@hey.com>
2023-05-08 00:04:07 +0200

    external API

---
+++ runtime/vm/src/api.cpp
@@ -0,0 +1,87 @@
+#include <thread>
+
+#include "mailbox/fiber.h"
+#include "api.h"
+
+vma::actor_system *CURRENT_ACTOR_SYSTEM = nullptr;
+vmm::fiber** CURRENT_FIBERS = nullptr;
+thread_count_t CURRENT_FIBERS_COUNT = 0;
+
+vma::actor *mkactor(vma::address addr, vma::address supervisor, vma::base_actor_state *state, vmm::mailbox *mb, vma::actor_type *type, vma::actor_system *system) {
+    return new vma::actor(addr, supervisor, std::unique_ptr<vma::base_actor_state>(state), std::shared_ptr<vmm::mailbox>(mb), std::shared_ptr<vma::actor_type>(type), std::shared_ptr<vma::actor_system>(system));
+}
+
+void dlactor(vma::actor *actor) {
+    delete actor;
+}
+
+void actor_receive(vma::actor *rcv, vmm::message *msg) {
+    rcv->push_from(std::unique_ptr<vmm::message>(msg));
+}
+
+void actor_send(vma::actor *actor, vmm::message *msg, vma::address &rcv) {
+    actor->send(std::unique_ptr<vmm::message>(msg), rcv);
+}
+
+void *actor_state(vma::actor *actor) {
+    auto state = actor->state_as<vma::base_actor_state>();
+    return static_cast<void *>(state.get());
+}
+
+vma::actor_system *mkactorsystem(thread_count_t count) {
+    auto actor_system = new vma::actor_system();
+
+    if (count == 0) {
+        count = std::thread::hardware_concurrency();
+        if (count == 0) {
+            count = 1;
+        } else {
+            count *= 2;
+        }
+    }
+
+    CURRENT_FIBERS_COUNT = count;
+    CURRENT_FIBERS = new vmm::fiber*[count];
+
+    auto using_system = [actor_system](vm::actor::address addr) -> std::shared_ptr<vm::actor::actor> {
+        return actor_system->resolve_by_address(addr);
+    };
+
+    for (auto i = 0; i < count; i++) {
+        auto mb = new vmm::mailbox();
+        CURRENT_FIBERS[i] = new vmm::fiber(std::shared_ptr<vmm::mailbox>(mb), using_system);
+    }
+
+    CURRENT_ACTOR_SYSTEM = actor_system;
+    return CURRENT_ACTOR_SYSTEM;
+}
+
+vma::actor_system *getactorsystem() {
+    return CURRENT_ACTOR_SYSTEM;
+}
+
+void dlactorsystem(vma::actor_system *system) {
+    for (auto i = 0; i < CURRENT_FIBERS_COUNT; i++) {
+        CURRENT_FIBERS[i]->stop();
+        delete CURRENT_FIBERS[i];
+    }
+
+    delete CURRENT_FIBERS;
+    delete CURRENT_ACTOR_SYSTEM;
+}
+
+vma::actor *actorsystem_resolve_by_address(vma::actor_system *system, const vma::address &addr) {
+    return system->resolve_by_address(addr).get();
+}
+
+void actorsystem_register_actor(vma::actor_system *system, vma::actor *actor) {
+    system->register_actor(std::shared_ptr<vma::actor>(actor));
+}
+
+vma::actor_type *mkactortype(const char *name) {
+    return new vma::actor_type(std::string(name));
+}
+
+void actortype_register(vma::actor_type *type, const char *msg, const vma::dispatch_message &dispatch) {
+    type->dispatch_table[std::string(msg)] = dispatch;
+}
---
+++ runtime/vm/src/api.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "./mailbox/mailbox.h"
+#include "./mailbox/fiber.h"
+#include "./core/address.h"
+#include "./actor/actor.h"
+#include "./actor/actor_type.h"
+#include "./actor/actor_system.h"
+
+namespace vma = vm::actor;
+namespace vmm = vm::mailbox;
+
+/**
+ * Sonata Runtime API
+ *
+ * This is an stable API that can be used within the compiler when generating code.
+ * This API can be linked both statically and dynamically with the application.
+ *
+ * Do not use this API directly: it's designed only for the compiler.
+ * If you want to integrate with the Sonata runtime at this level, use the C++ APIs.
+ */
+
+extern "C" {
+    typedef uint8_t thread_count_t;
+
+    // Actor API
+    vma::actor *mkactor(vma::address addr, vma::address supervisor, vma::base_actor_state *state, vmm::mailbox *mb, vma::actor_type *type, vma::actor_system *system);
+    void dlactor(vma::actor *actor);
+    void actor_receive(vma::actor *rcv, vmm::message *msg);
+    void actor_send(vma::actor *actor, vmm::message *msg, vma::address &rcv);
+    void *actor_state(vma::actor *actor); // actor state is only "typed" at the C++ level.
+
+    // Actor System API
+    vma::actor_system *mkactorsystem(thread_count_t count);
+    vma::actor_system *getactorsystem();
+    void dlactorsystem(vma::actor_system *system);
+    vma::actor *actorsystem_resolve_by_address(vma::actor_system *system, const vma::address &addr);
+    void actorsystem_register_actor(vma::actor_system *system, vma::actor *actor);
+
+    // Actor Type API
+    vma::actor_type *mkactortype(const char *name);
+    void actortype_register(vma::actor_type *type, const char *msg, const vma::dispatch_message &dispatch);
+}
